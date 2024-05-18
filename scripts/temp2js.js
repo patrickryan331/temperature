4d9560fe21af710c0a5f021e3285294960f43de7
Patrick Ryan <patrick@Patricks-MacBook-Pro.local>
2024-05-18 12:34:06 -0400

    assignment 3 temp conversion

---
+++ scripts/temp2js.js
@@ -0,0 +1,20 @@
+
+
+function convertTemperature(){
+
+    var scale = (prompt("Enter F for Fahrenheit or C for Celsuis"));
+    var temperature = Number(prompt("Enter the Temperature"));
+
+    if (scale=="F"){
+        
+        document.getElementById("results").innerHTML=(temperature) + " Fahrenheit converted to Celsius is " + (temperature - 32) * 5/9;
+    };
+    
+    if (scale=="C"){
+        document.getElementById("results").innerHTML=(temperature) + " Celcius converted to Fahrenheit is " + (temperature * 9/5) + 32;
+    };
+
+
+    }
+
+
